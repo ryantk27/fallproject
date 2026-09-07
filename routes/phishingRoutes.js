057d4d8e6c4321ab368e0a106e25ce73d4eb8d11
Ryan Toko <ryanmarktk@mac.mynetworksettings.com>
2026-09-07 19:25:37 -0400

    Remove project files except README

--- routes/phishingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const phishingController = require('../controllers/phishingController');
-
-router.post('/check', phishingController.checkUrl);
-router.post('/add', phishingController.addUrl);
-router.get('/all', phishingController.getAllUrls);
-
-module.exports = router;
